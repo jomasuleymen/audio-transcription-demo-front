47ca56d5e455348b1cbcd6daf8807bc0f9015314
jomasuleymen <joma.suleymen@gmail.com>
2025-08-29 14:41:31 +0500

    update App component to use ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
 import { ConfigProvider } from 'antd';
 
 import MainLayout from './layout/MainLayout';
 import Home from './pages/home/Home';
 
+export const client = new ApolloClient({
+	link: new HttpLink({ uri: import.meta.env.VITE_GRAPHQL_URL }),
+	cache: new InMemoryCache(),
+});
+
 const App: React.FC = () => {
 	return (
-		<ConfigProvider>
-			<MainLayout>
-				<Home />
-			</MainLayout>
-		</ConfigProvider>
+		<ApolloProvider client={client}>
+			<ConfigProvider>
+				<MainLayout>
+					<Home />
+				</MainLayout>
+			</ConfigProvider>
+		</ApolloProvider>
 	);
 };
 
